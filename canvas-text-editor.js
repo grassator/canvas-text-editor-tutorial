18fca6d0f18f93bdd2178ff6e87ab76ba70f755f
Dmitriy Kubyshkin <dmitriy@kubyshkin.ru>
2012-04-07 11:13:26 +0700

    Updated demo to use code from part 7.

--- canvas-text-editor.js
+++ canvas-text-editor.js
@@ -62,9 +62,14 @@ var CanvasTextEditor = function(doc) {
   this._document = doc || (new Document);
   this._metrics = new FontMetrics('"Courier New", Courier, monospace', 14);
   this._createWrapper();
+  this._selection = new Selection(this);
+  this._selection.onchange = this.selectionChange.bind(this);
   this._createCanvas();
   this._createInput();
-  this._selection = new Selection(this);
+  document.addEventListener('keydown', this.addKeyModifier.bind(this), true);
+  document.addEventListener('keyup', this.removeKeyModfier.bind(this), true);
+  window.addEventListener('focus', this.clearKeyModifiers.bind(this), true);
+  window.addEventListener('focus', this.render.bind(this), true);
 };
 
 module.exports = CanvasTextEditor;
@@ -74,6 +79,74 @@ module.exports = CanvasTextEditor;
  * @type {String}
  */
 CanvasTextEditor.prototype.className = 'canvas-text-editor';
+
+/**
+ * Determines if user holds shift key at the moment
+ * @type {Boolean}
+ */
+CanvasTextEditor.prototype.shiftPressed = false;
+
+/**
+ * Marks important for us key modfiers as pressed
+ * @param {Event} e
+ */
+CanvasTextEditor.prototype.addKeyModifier = function(e) {
+  if (e.keyCode === 16) {
+    this.shiftPressed = true;
+  }
+};
+
+/**
+ * Unmarks important for us key modfiers as pressed
+ * @param {Event} e
+ */
+CanvasTextEditor.prototype.removeKeyModfier = function(e) {
+  if (e.keyCode === 16) {
+    this.shiftPressed = false;
+  }
+};
+
+/**
+ * Clears all key modifiers
+ */
+CanvasTextEditor.prototype.clearKeyModifiers = function() {
+  this.shiftPressed = false;
+};
+
+/**
+ * Returns selection for this editor
+ * @return {Selection}
+ */
+CanvasTextEditor.prototype.getSelection = function() {
+  return this._selection;
+};
+
+/**
+ * Handles selection change
+ */
+CanvasTextEditor.prototype.selectionChange = function() {
+  // Assume that selection is empty
+  var selectedText = '';
+
+  // if it's not we put together selected text from document
+  if (!this._selection.isEmpty()) {
+    var ranges = this._selection.lineRanges(),
+        line;
+    for(var key in ranges) {
+      selectedText += this._document.getLine(parseInt(key)).slice(
+        ranges[key][0], ranges[key][1] === true ? undefined : ranges[key][1]
+      );
+    }
+  }
+
+  // Put selected text into our proxy
+  this.inputEl.value = selectedText;
+  this.inputEl.selectionStart = 0;
+  this.inputEl.selectionEnd = selectedText.length;
+
+  // Updating canvas to show selection
+  this.render();
+};
 
 /**
  * Creates wrapper element for all parts of the editor
@@ -113,18 +186,48 @@ CanvasTextEditor.prototype.render = function() {
       lineHeight = this._metrics.getHeight(),
       characterWidth = this._metrics.getWidth(),
       maxHeight = Math.ceil(640 / lineHeight),
-      lineCount = this._document.getLineCount();
+      lineCount = this._document.getLineCount(),
+      selectionRanges = this._selection.lineRanges(),
+      selectionWidth = 0;
 
   // Making sure we don't render somethign that we won't see
   if (lineCount < maxHeight) maxHeight = lineCount;
 
   // Clearing previous iteration
-  this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  this.context.fillStyle = '#eee';
+  this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  this.context.fillStyle = '#000';
 
   // Looping over document lines
   for(var i = 0; i < maxHeight; ++i) {
+    var topOffset = lineHeight * i;
+
+    // Rendering selection for this line if one is present
+    if (selectionRanges[i]) {
+      this.context.fillStyle = '#cce6ff';
+
+      // Check whether we should select to the end of the line or not
+      if(selectionRanges[i][1] === true) {
+        selectionWidth = this.canvas.width;
+      } else {
+        selectionWidth = (selectionRanges[i][1] - selectionRanges[i][0]) * characterWidth;
+      }
+
+      // Drawing selection
+      this.context.fillRect(
+        selectionRanges[i][0] * characterWidth,
+        i * lineHeight,
+        selectionWidth,
+        lineHeight
+      )
+
+      // Restoring fill color for the text
+      this.context.fillStyle = '#000';
+    }
+
+    // Drawing text
     this.context.fillText(
-      this._document.getLine(i), 0, lineHeight * i + baselineOffset
+      this._document.getLine(i), 0, topOffset + baselineOffset
     );
   }
 };
@@ -136,9 +239,10 @@ CanvasTextEditor.prototype.render = function() {
 CanvasTextEditor.prototype._createInput = function() {
   this.inputEl = document.createElement('textarea');
   this.inputEl.style.position = 'absolute';
-  this.inputEl.style.top = '-10px';
-  this.inputEl.style.height = 0;
-  this.inputEl.style.width = 0;
+  this.inputEl.style.top = '-25px';
+  this.inputEl.style.left = '-25px';
+  this.inputEl.style.height = '10px';
+  this.inputEl.style.width = '10px';
   this.inputEl.addEventListener('input', this.handleInput.bind(this), false);
   this.inputEl.addEventListener('blur', this.blur.bind(this), false);
   this.inputEl.addEventListener('focus', this._inputFocus.bind(this), false);
@@ -161,7 +265,14 @@ CanvasTextEditor.prototype.handleInput = function(e) {
  * @param  {string} text
  */
 CanvasTextEditor.prototype.insertTextAtCurrentPosition = function(text) {
+  // If selection is not empty we need to "replace" selected text with inserted
+  // one which means deleting old selected text before inserting new one
+  if (!this._selection.isEmpty()) {
+    this.deleteCharAtCurrentPosition();
+  }
+
   var pos = this._selection.getPosition();
+
   // Inserting new text and changing position of cursor to a new one
   this._selection.setPosition.apply(
     this._selection,
@@ -175,12 +286,23 @@ CanvasTextEditor.prototype.insertTextAtCurrentPosition = function(text) {
  * @param  {string} text
  */
 CanvasTextEditor.prototype.deleteCharAtCurrentPosition = function(forward) {
-  var pos = this._selection.getPosition();
-  // Deleting text and changing position of cursor to a new one
-  this._selection.setPosition.apply(
-    this._selection,
-    this._document.deleteChar(forward, pos[0], pos[1])
-  );
+  // If there is a selection we just remove it no matter what direction is
+  if (!this._selection.isEmpty()) {
+    this._selection.setPosition.apply(
+      this._selection,
+      this._document.deleteRange(
+        this._selection.start.character, this._selection.start.line,
+        this._selection.end.character, this._selection.end.line
+      )
+    );
+  } else {
+    var pos = this._selection.getPosition();
+    // Deleting text and changing position of cursor to a new one
+    this._selection.setPosition.apply(
+      this._selection,
+      this._document.deleteChar(forward, pos[0], pos[1])
+    );
+  }
   this.render();
 };
 
@@ -230,7 +352,8 @@ CanvasTextEditor.prototype.resize = function(width, height) {
 };
 
 /**
- * Main keydown handler.
+ * Main keydown handler
+ * @param  {Event} e
  */
 CanvasTextEditor.prototype.keydown = function(e) {
   var handled = true;
@@ -245,16 +368,16 @@ CanvasTextEditor.prototype.keydown = function(e) {
       this.insertTextAtCurrentPosition('\n');
       break;
     case 37: // Left arrow
-      this._selection.moveLeft();
+      this._selection.moveLeft(1, this.shiftPressed);
       break;
     case 38: // Up arrow
-      this._selection.moveUp();
+      this._selection.moveUp(1, this.shiftPressed);
       break;
     case 39: // Up arrow
-      this._selection.moveRight();
+      this._selection.moveRight(1, this.shiftPressed);
       break;
     case 40: // Down arrow
-      this._selection.moveDown();
+      this._selection.moveDown(1, this.shiftPressed);
       break;
     default:
       handled = false;
@@ -268,7 +391,6 @@ CanvasTextEditor.prototype.keydown = function(e) {
  * Blur handler.
  */
 CanvasTextEditor.prototype.blur = function() {
-  this.inputEl.blur();
   this.wrapper.style.outline = 'none';
   this._selection.setVisible(false);
 };
@@ -400,7 +522,7 @@ Document.prototype.deleteRange = function(startColumn, startRow, endColumn, endR
   startRow >= 0 || (startRow = 0);
   startColumn >= 0 || (startColumn = 0);
   endRow < this.storage.length || (endRow = this.storage.length - 1);
-  endColumn <= this.storage[endRow].length || (endColumn = this.storage[endRow].length);
+  endColumn <= this.storage[endRow].trim('\n').length || (endColumn = this.storage[endRow].length);
 
   // Little optimization that does nothing if there's nothing to delete
   if(startColumn === endColumn && startRow === endRow) {
@@ -430,14 +552,18 @@ Document.prototype.deleteChar = function(forward, startColumn, startRow) {
       endColumn = startColumn;
 
   if (forward) {
+    var characterCount = this.storage[startRow].trim('\n').length;
     // If there are characters after cursor on this line we simple remove one
-    if (startColumn < this.storage[startRow].trim('\n').length) {
+    if (startColumn < characterCount) {
       ++endColumn;
     }
     // if there are rows after this one we append it
-    else if (startRow < this.storage.length - 1) {
-      ++endRow;
-      endColumn = 0;
+    else {
+      startColumn = characterCount;
+      if (startRow < this.storage.length - 1) {
+        ++endRow;
+        endColumn = 0;
+      }
     }
   }
   // Deleting backwards
@@ -475,8 +601,9 @@ var FontMetrics = function(family, size) {
   body.appendChild(line);
 
   // Now we can measure width and height of the letter
-  line.innerHTML = 'm'; // It doesn't matter what text goes here
-  this._width = line.offsetWidth;
+  var text = 'mmmmmmmmmm'; // 10 symbols to be more accurate with width
+  line.innerHTML = text;
+  this._width = line.offsetWidth / text.length;
   this._height = line.offsetHeight;
 
   // Now creating 1px sized item that will be aligned to baseline
@@ -542,7 +669,6 @@ FontMetrics.prototype.getBaseline = function() {
  */
 Selection = function(editor) {
   this.editor = editor;
-  this.blinkInterval = 500;
 
   this.start = {
     line: 0,
@@ -565,6 +691,24 @@ Selection = function(editor) {
 };
 
 /**
+ * Hold blink interval for the cursor
+ * @type {Number}
+ */
+Selection.prototype.blinkInterval = 500;
+
+/**
+ * This callback called when selection size has changed
+ * @type {Function}
+ */
+Selection.prototype.onchange = null;
+
+/**
+ * If true that means that we currently manipulate right side of the selection
+ * @type {Boolean}
+ */
+Selection.prototype.activeEndSide = true;
+
+/**
  * Responsible for blinking
  * @return {void}
  */
@@ -577,33 +721,111 @@ Selection.prototype.blink = function() {
 };
 
 /**
- * Moves both start and end to a specified position inside document.
- * @param {number?} line
- * @param {number?} character
+ * Returns selection split into line ranges
+ * @return {Array}
  */
-Selection.prototype.setPosition = function(character, line) {
-  // Providing defaults for both line and character parts of position
-  if (typeof line === 'undefined') line = this.end.line
-  if (typeof character === 'undefined') character = this.end.character
+Selection.prototype.lineRanges = function() {
+  if (this.isEmpty()) return {};
+  var ranges = {},
+      character = this.start.character,
+      line = this.start.line;
+  for (; line <= this.end.line ; line++) {
+    ranges[line] = ([character, line !== this.end.line || this.end.character]);
+    character = 0;
+  };
+  return ranges;
+};
+
+/**
+ * Comparator for two cursor positions
+ * @return {number}
+ */
+Selection.prototype.comparePosition = function(one, two) {
+  if (one.line < two.line) {
+    return -1;
+  } else if (one.line > two.line) {
+    return 1;
+  } else {
+    if (one.character < two.character) {
+      return -1;
+    } else if (one.character > two.character) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+};
+
+/**
+ * Determines if selection is emtpy (zero-length)
+ * @return {boolean}
+ */
+Selection.prototype.isEmpty = function() {
+  return this.comparePosition(this.start, this.end) === 0;
+};
+
+/**
+ * Moves both start and end to a specified position inside document.
+ * @param {number} line
+ * @param {number} character
+ */
+Selection.prototype.setPosition = function(character, line, keepSelection) {
+
+  var position = this._forceBounds(character, line);
+
+  // Calling private setter that does the heavy lifting
+  this._doSetPosition(position[0], position[1], keepSelection);
+  this._updateCursorStyle();
+
+  // Making a callback if necessary
+  if (typeof this.onchange === 'function') {
+    this.onchange(this, this.start, this.end);
+  }
+};
+
+/**
+ * Checks and forces bounds for proposed position updates
+ * @return {Array}
+ */
+Selection.prototype._forceBounds = function(character, line) {
+  var position = this.getPosition();
 
   // Checking lower bounds
   line >= 0 || (line = 0);
-  character >= 0 || (character = 0);
+  if (character < 0) {
+    // Wraparound for lines
+    if (line === position[1] && line > 0) {
+      --line;
+      character = this.editor.getDocument().getLine(line).trim('\n').length;
+    } else {
+      character = 0;
+    }
+  }
 
   // Checking upper bounds
   var lineCount = this.editor.getDocument().getLineCount();
   line < lineCount || (line = lineCount - 1);
   var characterCount = this.editor.getDocument().getLine(line).trim('\n').length;
-  character <= characterCount || (character = characterCount);
+  if (character > characterCount) {
+    if (line === position[1] && line < this.editor.getDocument().getLineCount() - 1) {
+      ++line;
+      character = 0;
+    } else {
+      character = characterCount;
+    }
+  }
+  return [character, line];
+};
 
-  // Saving new value
-  this.start.line = this.end.line = line;
-  this.start.character = this.end.character = character;
-
+/**
+ * Updates cursor styles so it matches current position
+ */
+Selection.prototype._updateCursorStyle = function() {
   // Calculating new position on the screen
   var metrics = this.editor.getFontMetrics(),
-      offsetX = character * metrics.getWidth(),
-      offsetY = line * metrics.getHeight();
+      position = this.getPosition(),
+      offsetX = position[0] * metrics.getWidth(),
+      offsetY = position[1] * metrics.getHeight();
   this.el.style.left = offsetX + 'px';
   this.el.style.top = offsetY + 'px';
 
@@ -618,47 +840,102 @@ Selection.prototype.setPosition = function(character, line) {
 };
 
 /**
+ * Private unconditional setter for cursor position
+ * @param  {number} character
+ * @param  {number} line
+ * @param  {boolean} keepSelection
+ */
+Selection.prototype._doSetPosition = function(character, line, keepSelection) {
+  // Saving new value
+  if (keepSelection) {
+
+    compare = this.comparePosition({
+      line: line,
+      character: character
+    }, this.start);
+
+    // If selection is empty and we are moving left we set active side to start
+    if (compare === -1 && (this.isEmpty() || line < this.start.line)) {
+      this.activeEndSide = false;
+    } 
+
+    if (this.activeEndSide) {
+      this.end.line = line;
+      this.end.character = character;
+    } else {
+      this.start.line = line;
+      this.start.character = character;
+    }
+
+    // Making sure that end is further than start
+    if (this.comparePosition(this.start, this.end) > 0) {
+      this.activeEndSide = !this.activeEndSide;
+      var temp = {
+        line: this.start.line,
+        character: this.start.character
+      }
+      this.start.line = this.end.line;
+      this.start.character = this.end.character;
+      this.end.line = temp.line;
+      this.end.character = temp.character;
+    }
+  } else {
+    this.activeEndSide = true;
+    this.start.line = this.end.line = line;
+    this.start.character = this.end.character = character;
+  }
+};
+
+/**
  * Returns current position of the end of the selection
  * @return {Array}
  */
 Selection.prototype.getPosition = function() {
-  return [this.end.character, this.end.line];
+  if (this.activeEndSide) {
+    return [this.end.character, this.end.line];
+  } else {
+    return [this.start.character, this.start.line];
+  }
 }
 
 /**
  * Moves up specified amount of lines.
  * @param  {number} length
  */
-Selection.prototype.moveUp = function(length) {
+Selection.prototype.moveUp = function(length, keepSelection) {
   arguments.length || (length = 1);
-  this.setPosition(this.end.character, this.end.line - length);
+  var position = this.getPosition();
+  this.setPosition(position[0], position[1] - length, keepSelection);
 };
 
 /**
  * Moves down specified amount of lines.
  * @param  {number} length
  */
-Selection.prototype.moveDown = function(length) {
+Selection.prototype.moveDown = function(length, keepSelection) {
   arguments.length || (length = 1);
-  this.setPosition(this.end.character, this.end.line + length);
+  var position = this.getPosition();
+  this.setPosition(position[0], position[1] + length, keepSelection);
 };
 
 /**
  * Moves up specified amount of lines.
  * @param  {number} length
  */
-Selection.prototype.moveLeft = function(length) {
+Selection.prototype.moveLeft = function(length, keepSelection) {
   arguments.length || (length = 1);
-  this.setPosition(this.end.character - length, this.end.line);
+  var position = this.getPosition();
+  this.setPosition(position[0] - length, position[1], keepSelection);
 };
 
 /**
  * Moves down specified amount of lines.
  * @param  {number} length
  */
-Selection.prototype.moveRight = function(length) {
+Selection.prototype.moveRight = function(length, keepSelection) {
   arguments.length || (length = 1);
-  this.setPosition(this.end.character + length, this.end.line);
+  var position = this.getPosition();
+  this.setPosition(position[0] + length, position[1], keepSelection);
 };
 
 /**
